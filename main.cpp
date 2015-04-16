0e870b8472d0d1242931db391e44f6ec0a1d43c0
amfipter <amfipter@gmail.com>
2015-04-16 11:50:30 +0600

    research changes. uncomplete

--- main.cpp
+++ main.cpp
@@ -5,26 +5,61 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "ESOINN.h"
 #include <boost/numeric/ublas/vector.hpp>
+#include <stdlib.h>
 
-typedef boost::numeric::ublas ub;
+// typedef boost::numeric::ublas ub;
+using namespace boost::numeric::ublas;
 
-int main() {
-    soinn::ESOINN model(4, 4, 30);
-    std::ifstream ifs;
-    ifs.open("input.txt");
-    ifs.open("iris.txt");
-    ub::vector<double> input(4);
+// void update_cluster_vectors(std::vector<vector<double>>&, soinn::ESOINN&);
+
+int main(int argc, char** argv) {
+    soinn::ESOINN model(34671, 10, 30);
+    std::ifstream ifs_train;
+    std::ifstream ifs_test;
+    std::ofstream ofs_train;
+    std::ofstream ofs_test;
+    // std::vector<vector<double>> clusters;
+    std::vector<int> ans;
+
+    if(argc < 7) {
+        printf("\tWrong args.\n");
+        printf("Use:\n ./ESOINN [train_file_name] [test_file_name] [train_file_size] [test_file_size] [output_train_set_name] [output_test_set_name]\n");
+        return 0;
+    }
+
+    ifs_train.open(argv[1]);
+    ifs_test.open(argv[2]);
+    ofs_train.open(argv[5]);
+    ofs_test.open(argv[6]);
+    // ifs.open("iris.txt");
+    vector<double> input(34671);
     int num;
-    int tmp;
-    for(int t = 0; t < 150; t++) {
-        for(int i = 0; i < 4; i++) {
-            ifs>>tmp;
-            input(i) = double(tmp);
+    double tmp;
+    int train_size = atoi(argv[3]);
+    int test_size = atoi(argv[4]);
+    for(int t = 0; t < train_size; t++) {
+        ifs >> num;
+        ans.push_back(num);
+        for(int i = 0; i < 34671; i++) {
+            ifs >> tmp;
+            input(i) = tmp;
         }
+        fprintf(stderr, "\r%d   ", t);
         model.process(input);
-        std::cout<<t<<std::endl;
+        // std::cout<<t<<std::endl;
     }
     model.classify();
     std::cout<<"number of classes: "<<model.getNumberOfClasses()<<std::endl;
     std::cout<<"number of vertices: "<<model.getNumberOfVertices()<<std::endl;
+
+    ifs_train.close();
+    ifs_train.open(argv[1]);
+    
 }
+
+// void update_cluster_vectors(std::vector<vector<double>>& clusters, soinn::ESOINN& model) {
+//     int cluster_count = model.getNumberOfClasses();
+//     for(i=0; i<cluster_count; i++) {
+//         clusters.push_back(model.)
+//     }
+// }
